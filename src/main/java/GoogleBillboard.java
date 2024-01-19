6018d621846f7e37749565c4e14084296cf09f41
WiChen7 <131711524+WiChen7@users.noreply.github.com>
2024-01-18 23:25:02 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,31 +2,36 @@
 
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		for (int i = 0; i < e.length()-10; i++) {
-    String digits = e.substring(i, i+10);
-    double dNum = Double.parseDouble(digits);
-    if (isPrime(dNum) == true) {
-      System.out.println(dNum);
-    }
-  }
-	}
-	
 
-	//Finish this function
-	public static boolean isPrime(double dNum)  
-{   
-  if (dNum < 2.0) {
-    return false;
+public void setup()  
+{    
+   String digits;
+   double dNum;
+    for (int i = 12; i <e.length(); i++){
+    digits = e.substring(i-10,i);
+    dNum = Double.parseDouble(digits);
+     if (isPrime(dNum)==false){
+       System.out.println(dNum);
+       break;
+     }
   }
-  for (int i = 2; i <= Math.sqrt(dNum); i++) {
-    if (dNum%i == 0) {
-      return false;
-    }
-  }
-  //your code here   
-  return true;
-} 
+   
+}  public void draw()  
+{  
+  //not needed for this assignment
+}  
+public boolean isPrime(double dNum)  
+{  
+  boolean True = false;
+     for (int j = 2; j <= Math.sqrt(dNum); j++){
+       if (dNum%j==0)
+     True=true;
+   }
+   if (True==false)
+  return false;
+ return true;
+
+ } 
 
 }
 
